e86ab4d3fafce226a9883c5fc80445ac0beae49a
Ajay-at-github <146206087+Ajay-at-github@users.noreply.github.com>
2024-05-18 20:49:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,40 @@
-from openai import OpenAI
+import openai
+import streamlit as st
 
-YOUR_API_KEY = "sk-proj-UOsqNlzsb8E5rDnYWwHwT3BlbkFJD3WvXyg4FoaRVn9VbkSf"
+st.title("ChatGPT-like clone")
 
-messages = [
-    {
-        "role": "system",
-        "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
-        ),
-    },
-    {
-        "role": "user",
-        "content": (
-            "How many stars are in the universe?"
-        ),
-    },
-]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4"
 
-# chat completion without streaming
-response = client.chat.completions.create(
-    model="llama-3-sonar-large-32k-online",
-    messages=messages,
-)
-print(response)
+print(st.session_state["openai_model"])  # Debugging
 
-# chat completion with streaming
-response_stream = client.chat.completions.create(
-    model="llama-3-sonar-large-32k-online",
-    messages=messages,
-    stream=True,
-)
-for response in response_stream:
-    print(response)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": full_response})
